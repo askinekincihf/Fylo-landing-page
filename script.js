23a3319eeac1d0f25564ec203b462adfb40de423
askinekinci <askinekincihf@gmail.com>
2020-12-18 23:15:22 +0000

    created header and main-first section

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    const mobilemenu = document.getElementById("menu");
+    if (mobilemenu.style.display === "block") {
+        mobilemenu.style.display = "none";
+    } else {
+        mobilemenu.style.display = "block";
+    }
+}
